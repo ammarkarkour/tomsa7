a35b77ca68399f19817486b4d0c8bf0801eee96b
ammarkarkour <amar.karkour@gmail.com>
2021-05-24 05:00:06 +0300

    Add: project tomsa7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Remarkable from 'remarkable';
+import { Box, Textarea, Text, HStack, VStack  } from "@chakra-ui/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.md = new Remarkable();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: ''};
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  getRawMarkup() {
+    return { __html: this.md.render(this.state.value) };
+  }
+
+  render() {
+    return (
+	    <>
+        <VStack spacing="10px">
+          
+          <HStack spacing="275px">
+            <Text fontSize="30px" color="black">
+              Input
+            </Text>
+
+            <Text fontSize="30px" color="black">
+              Output
+            </Text>
+          </HStack>
+
+          <HStack spacing="50px">
+            <Textarea
+              w="300px"
+              h="200px"
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder="Input please!"
+              size="md"
+            />
+
+    	      <Box bg="#68D391" w="300px" h="200px" p={4} color="white">
+      	      <div
+                className="content"
+                dangerouslySetInnerHTML={this.getRawMarkup()}
+      	      />
+      		  </Box>
+          </HStack>
+        
+        </VStack>
+
+	    </>
+    );
+  }
 }
 
-export default App;
+
+export default MarkdownEditor;
